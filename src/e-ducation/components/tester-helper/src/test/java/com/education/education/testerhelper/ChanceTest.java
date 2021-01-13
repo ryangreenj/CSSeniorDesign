1ebdf0dd8ffadd0c32eb6e605d37e9a55db14ae7
jakeocinco <jakeocinco@me.com>
2021-01-13 13:55:14 -0500

    Test helper classes

---
+++ src/e-ducation/components/tester-helper/src/test/java/com/education/education/testerhelper/ChanceTest.java
@@ -0,0 +1,31 @@
+package com.education.education.testerhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {Chance.class})
+class ChanceTest {
+
+    @Test
+    void getRandomAlphaNumericString_shouldReturnStringOfDesiredLength() {
+        final int length = new Random().nextInt(100);
+        final String s = Chance.getRandomAlphaNumericString(length);
+
+        assertThat(s.length()).isEqualTo(length);
+    }
+
+    @Test
+    void getRandomNumberBetween_shouldReturnANumberWithinBounds() {
+        final int l_bound = new Random().nextInt(Integer.MAX_VALUE / 2);
+        final int u_bound = new Random().nextInt(Integer.MAX_VALUE / 2) + l_bound;
+
+        final int actual = Chance.getRandomNumberBetween(l_bound, u_bound);
+
+        assertThat(actual).isGreaterThanOrEqualTo(l_bound);
+        assertThat(actual).isLessThanOrEqualTo(u_bound);
+    }
+}
