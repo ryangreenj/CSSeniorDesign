fed036b8efb8e415ff045846e2c938118a8e1466
Ryan <ryangreen105+siemens@gmail.com>
2021-02-02 22:04:14 -0500

    Update data structures closer to back-end, add Session data to shared service

--- src/e-ducation-front-end/src/app/data.service.ts
+++ src/e-ducation-front-end/src/app/data.service.ts
@@ -1,3 +1,4 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
@@ -14,25 +15,34 @@ export class DataService {
   changeData(data: SharedData) {
     this.dataSource.next(data);
   }
+  
+  getSessionData(sessionId: string) {
+    // Load session data from ID
+    return { "courseId": "12334123", "sessionName": sessionId + " Session" };
+  }
 }
 
 export class SharedData {
   // Temporarily fill, this service is where we can load and store shared class data
   classes: ClassData[] = [
-    { "id": '100', "code": "CS101", "name": "Computer Science 1", "status": false },
-    { "id": '101', "code": "EECE3093C", "name": "Software Engineering", "status": false },
-    { "id": '102', "code": "COMM1017", "name": "Intro to Public Speaking", "status": false },
-    { "id": '103', "code": "SPN1001", "name": "Spanish 1", "status": true }
+    { "id": "100", "code": "CS101", "className": "Computer Science 1", "status": false, "sessionIds": [] },
+    { "id": "101", "code": "EECE3093C", "className": "Software Engineering", "status": true, "sessionIds": [] },
+    { "id": "102", "code": "COMM1017", "className": "Intro to Public Speaking", "status": false, "sessionIds": [] },
+    { "id": "103", "code": "SPN1001", "className": "Spanish 1", "status": true, "sessionIds": [] },
+    { "id": "104", "code": "MATH1062", "className": "Calculus 2", "status": false, "sessionIds": ["123", "234", "456"] }
   ];
   
-  activeClass: ClassData;
+  currentClass: ClassData;
+  currentSessionId: string;
+  currentSession: Session;
 }
 
 export type ClassData = {
   id: string
   code: string
-  name: string
+  className: string
   status: boolean
+  sessionIds: string[]
 }
 
 export type Session = {
