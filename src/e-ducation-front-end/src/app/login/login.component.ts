dc1bea5c1813de41c61c42fb3340c9c12a25ae29
Ryan <ryangreen105+siemens@gmail.com>
2021-02-20 18:48:52 -0500

    Remove bad import

--- src/e-ducation-front-end/src/app/login/login.component.ts
+++ src/e-ducation-front-end/src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ExecOptionsWithStringEncoding } from 'child_process';
 import { DataService } from '../data.service';
 
 @Component({
